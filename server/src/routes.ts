a7a4740f87e57ec83e97cb143405fc7aa9463f0b
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-08-06 08:43:25 -0300

    Listing connections and filter classes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import ClassController from './controllers/ClassesControllers';
+import ConnectionsController from './controllers/ConnectionsControllers';
 
 const routes = express.Router();
 
 const classesControllers = new ClassController();
+const connectionsControllers = new ConnectionsController();
 
+routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
 export default routes;

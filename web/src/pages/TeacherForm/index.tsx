51cfd6d0e7e9d7308cc48094cd240292b5811480
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-08-06 21:26:49 -0300

    Get data total connections from api to load in main page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import './styles.css';
 import Input from '../../components/Input';
 import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 function TeacherForm() {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: '', to: '' }
+  ]);
+
+  function addNewScheduleitem() {
+    setScheduleItems([
+      ...scheduleItems,
+      { week_day: 0, from: '', to: '' },
+    ]);
+  };
+
   return (
     <div className="container" id="page-teacher-form">
       <PageHeader 
@@ -19,12 +32,56 @@ function TeacherForm() {
             <Input name="name" label="Nome completo" />
             <Input name="avatar" label="Avatar" />
             <Input name="whatsapp" label="Whatsapp" />
+            <Textarea name="bio" label="Biogradia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
-            <Input name="subject" label="Matéria" />
+            <Select 
+              name="subject" 
+              label="Matéria"
+              options={[
+                { value: 'Artes', label: 'Artes'},
+                { value: 'Biologia', label: 'Biologia'},
+                { value: 'Geografia', label: 'Geografia'},
+                { value: 'Português', label: 'Português'},
+                { value: 'Inglês', label: 'Inglês'}
+              ]}
+            />
+
             <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Horários disponpiveis
+            <button type="button" onClick={addNewScheduleitem}>
+            + Novo horário
+            </button>
+          </legend>
+
+          {scheduleItems.map(scheduleItem => {
+            return (
+              <div key={scheduleItem.week_day} className="schedule-item">
+                <Select 
+                  name="week_day" 
+                  label="Dia da semana"
+                  options={[
+                    { value: '0', label: 'Domingo'},
+                    { value: '1', label: 'Segunda-feira'},
+                    { value: '2', label: 'Terça-feira'},
+                    { value: '3', label: 'Quarta-feira'},
+                    { value: '4', label: 'Quinta-feira'},
+                    { value: '5', label: 'Sexta-feira'},
+                    { value: '6', label: 'Sábado'},
+                  ]}
+                />
+
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+              </div>
+            );
+          })}
+          
         </fieldset>
 
         <footer>
